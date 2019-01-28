c51672efa3aa18a85656e500c115b4c760a48109
ashishpatel310 <46492064+ashishpatel310@users.noreply.github.com>
2019-01-27 22:42:41 -0500

    loop through sorted products

--- groceries.py
+++ groceries.py
@@ -21,8 +21,19 @@ products = [
     {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
     {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+]
 
-print(products)
+#print(products)
 
-# TODO: write some Python code here to produce the desired output
+#print(products[0]["name"])
+
+def product_name(any_product):
+    return any_product["name"]
+
+products = sorted(products, key=product_name)
+
+for p in products:
+    print(p["name"] + " Price = $" + str(p["price"]))
+
+for p in products:
+    print(p["department"])
