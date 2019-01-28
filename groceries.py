f167e3ada9e857aeda827b289422a6c812667eb0
ashishpatel310 <46492064+ashishpatel310@users.noreply.github.com>
2019-01-28 00:36:43 -0500

    Alternative way to remove duplicate departments

--- groceries.py
+++ groceries.py
@@ -42,16 +42,20 @@ departments = []
 
 for p in products:
     #print(p["department"])
-    #departments.append(p["department"])
-    if p["department"] not in departments:
-        departments.append(p["department"])
+    departments.append(p["department"])
+    #if p["department"] not in departments:
+    #    departments.append(p["department"])
 
-department_count = len(departments)
+
+unique_departments = list(set(departments)
+
+
+department_count = len(unique_departments)
 
 
 print("--------------")
 print("THERE ARE " + str(department_count) + " DEPARTMENTS:")
 print("--------------")
 
-for d in departments:
+for d in unique_departments:
     print(d)
